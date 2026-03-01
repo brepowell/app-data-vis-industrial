9aeb971e518e91ef5ac8f22feef444ad48d4bde0
Breanna Powell <85724630+brepowell@users.noreply.github.com>
2026-02-28 18:35:00 -0800

    Load SECOM dataset from CSV in streamlit app

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,6 @@ import pandas as pd
 st.title('⚡SECOM Industrial Dataset Visualization')
 
 st.write('This app allows you to explore the SECOM dataset')
+
+df = pd.read_csv("https://raw.githubusercontent.com/brepowell/app-data-vis-industrial/refs/heads/master/secom_combined.csv")
+df
